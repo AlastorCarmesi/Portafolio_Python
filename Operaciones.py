99dcac38639b2b8c86f166c3c6a0441288998d82
AlastorCarmesi <carmesiblog7@gmail.com>
2023-06-20 12:43:44 -0600

    Tercera practica

--- Operaciones.py
+++ Operaciones.py
@@ -9,12 +9,10 @@ RestNeg = Num2 - Num1
 Mult = Num1 * Num2
 Div = Num2/Num1
 Porc = Num1 % Num2
-
-Oper_Compl = Num2+23*56+Num1
-
+print((Num2+23)*56+Num1)
 Exp = (Num1**3) #Este tipo de operacion pertenece al cubo o al cuadrado
+Div_NoFloat = Num2 // Num1 #Permite eliminar un numero decimal dando un numero entero
 
-Div_NoFloat = Num2 // Num1
 
 #Resultados
 print("Suma:", Sum, 
@@ -24,4 +22,4 @@ print("Suma:", Sum,
       "Division: ", Div,
       "Porcentaje:", Porc)
 
-print("Resultado de operacion compleja:",Div_NoFloat)
+print("Resultado de operacion compleja:", Porc)
