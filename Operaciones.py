ad08b6c42dd027f86f7b65f89243a4721d620395
AlastorCarmesi <carmesiblog7@gmail.com>
2023-06-20 12:21:52 -0600

    Tercera practica

---
+++ Operaciones.py
@@ -0,0 +1,12 @@
+Sum = 43+100
+Rest = 100-43
+RestNeg = 43-100
+Mult = 100*43
+Div = 100/5
+
+#Resultados
+print("Suma:", Sum, 
+      "Rest", Rest, 
+      "Resta con resultado negativo: ",RestNeg,
+      "Multiplicacion: ",Mult, 
+      "Division: ", Div)
