6c270a0fe79e3fafe43d8de74429a64150ae93ab
AlastorCarmesi <carmesiblog7@gmail.com>
2023-06-20 12:34:22 -0600

    Tercera practica

--- Operaciones.py
+++ Operaciones.py
@@ -1,8 +1,19 @@
-Sum = 43+100
-Rest = 100-43
-RestNeg = 43-100
-Mult = 100*43
-Div = 100/5
+#Operaciones con variables
+Num1 = 2
+Num2 = 3
+
+#Operaciones
+Sum = Num2 + Num1
+Rest = Num1 - Num2
+RestNeg = Num2 - Num1
+Mult = Num1 * Num2
+Div = Num2/Num1
+
+Oper_Compl = Num2+23*56+Num1
+
+Exp = (Num1**3) #Este tipo de operacion pertenece al cubo o al cuadrado
+
+Div_NoFloat = Num2 // Num1
 
 #Resultados
 print("Suma:", Sum, 
@@ -10,3 +21,5 @@ print("Suma:", Sum,
       "Resta con resultado negativo: ",RestNeg,
       "Multiplicacion: ",Mult, 
       "Division: ", Div)
+
+print("Resultado de operacion compleja:",Div_NoFloat)
