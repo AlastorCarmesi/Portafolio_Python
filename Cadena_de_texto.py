b52d36f135d135b27ffbe80e7c5ee4f3417c3a3a
AlastorCarmesi <carmesiblog7@gmail.com>
2023-06-27 14:57:03 -0600

    Quinta practica

--- Cadena_de_texto.py
+++ Cadena_de_texto.py
@@ -1,3 +1,5 @@
-cadena = 'Texto \'de\' ejemplo' #Usar la diagonal invertida
-cadena2 = 'Texto \nde ejemplo de salto de linea' #Usar la diagonal invertida
-print(cadena2)
+cadena0 = "Texto de ejemplo\n hola mundo"
+cadena1 = 'Texto \'de\' ejemplo' #Usar la diagonal invertida para agregar signos
+cadena2 = 'Texto \nde ejemplo de salto de linea' #Usar la diagonal invertida y n para realizar un salto de linea
+cadena3 = 'Texto de \t ejemplo de tabulacion' #uso de diagonal invertida y t para tabular
+print(cadena3)
