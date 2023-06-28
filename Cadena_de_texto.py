eb3bd5da2c5b8e7c22c32c7e84b5733662515ba7
AlastorCarmesi <carmesiblog7@gmail.com>
2023-06-28 12:02:24 -0600

    Quinta practica

--- Cadena_de_texto.py
+++ Cadena_de_texto.py
@@ -1,5 +1,23 @@
-cadena0 = "Texto de ejemplo\n hola mundo"
+cadena0 = "texto de ejemplo hola mundo" #Se pueden usar estos comandos dentro de cadenas entre ""
 cadena1 = 'Texto \'de\' ejemplo' #Usar la diagonal invertida para agregar signos
 cadena2 = 'Texto \nde ejemplo de salto de linea' #Usar la diagonal invertida y n para realizar un salto de linea
 cadena3 = 'Texto de \t ejemplo de tabulacion' #uso de diagonal invertida y t para tabular
-print(cadena3)
+cadena4 = 'Hola '
+cadena5 = 'Mundo'
+
+print("Ejemplos de comandos: ", cadena3)
+
+#Suma aritmetica con cadenas 
+print("Combinacion de cadenas: ", cadena4 + cadena5)
+
+#Multiplicacion aritmetica con cadenas
+print("Multiplicacion: ",cadena5 * 5)
+
+#Debanado de cadenas 
+print("Debanado de cadena: ", cadena0[0 : 5]) #permite asignar la poscicion de 0 a X depende de la cantidad de la cadena y obtiene el valor como tambien los espacios
+
+#Metodos aplicados en cadenas 
+print("Minuscula: ", cadena0.lower()) #Convierte en minusculas a la cadena
+print("Mayuscula: ", cadena0.upper()) #Convierte en Mayusculas a la cadena
+print("Conversion: ", cadena0.capitalize()) #convierte la primera letra de la cadena en mayuscula 
+print("Titulo: ", cadena0.title()) #en cada letra inicial de una palabra los convierte en mayuscula
