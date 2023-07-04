5f95ed23ff50678cbf3654d4312d1de142c3b78c
AlastorCarmesi <carmesiblog7@gmail.com>
2023-07-03 19:18:52 -0600

    Septima practica

---
+++ Ingreso_de_datos.py
@@ -0,0 +1,4 @@
+Nombre = input("Ingresa tu nombre: ") #Nos permite ingresar algun tipo de digito de tipo String
+print("este es tu nombre:", Nombre) #Muestra el resultado de lo que tecleamos
+Edad = int(input("Cual es tu edad?")) #Al momento de ingresar un numero debemos especificar de que tipo pertenece
+print("Tienes", Edad, "Años") #Muestra el resultado de lo que tecleamos
