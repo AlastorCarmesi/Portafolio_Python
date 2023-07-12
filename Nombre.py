94e3c3571d67b4282f83d956ec1c392c9b7c2281
AlastorCarmesi <carmesiblog7@gmail.com>
2023-07-12 12:06:43 -0600

    Practica 10

---
+++ Nombre.py
@@ -0,0 +1,14 @@
+'''
+En esta practica ingresaremos el nombre, la edad y el sexo de un usuario
+'''
+
+Nom = input('Tu nombre: ')
+Edad = int(input('tu edad: '))
+Sex = input('Cual es tu genero: ')
+
+'''
+Mostramos los formatos de texto
+'''
+print("Tu nombre es:\n",Nom)
+print("Este es tu Sexo:\n",Sex)
+print("esta es tu edad:\n",Edad)
